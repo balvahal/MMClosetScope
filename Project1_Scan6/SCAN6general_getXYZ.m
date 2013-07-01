6dacd09aa2c34613175f9659a7460f4d987463ac
Kyle Wayne Karhohs <gandalfisarockstar@gmail.com>
2013-07-01 17:04:33 -0400

    first commit

---
+++ Project1_Scan6/SCAN6general_getXYZ.m
@@ -0,0 +1,13 @@
+%% Get x, y, and z position of microscope
+% 
+%% Inputs
+% * mmhandle, the struct that contains micro-manager objects
+%% Outputs
+% * mmhandle, the struct that contains micro-manager objects
+function [mmhandle] = SCAN6general_getXYZ(mmhandle)
+%% retrieve x, y, and z
+%
+x = mmhandle.core.getXPosition(mmhandle.xyStageDevice);
+y = mmhandle.core.getYPosition(mmhandle.xyStageDevice);
+z = mmhandle.core.getPosition(mmhandle.FocusDevice);
+mmhandle.pos = [x,y,z];
