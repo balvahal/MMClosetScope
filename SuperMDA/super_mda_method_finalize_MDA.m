67e16b0248eecef43821aaf133c7d720b56781a1
Kyle Wayne Karhohs - closet scope <gandalfisarockstar@gmail.com>
2014-03-04 14:20:14 -0500

    fixing pre-allocation issues

--- SuperMDA/super_mda_method_finalize_MDA.m
+++ SuperMDA/super_mda_method_finalize_MDA.m
@@ -1,18 +1,6 @@
 %%
 %
 function [obj] = super_mda_method_finalize_MDA(obj)
-%%
-% initialize the table that will store the history of
-% the MDA.
-obj.database = table;
-
-SuperMDAtable = table(...
-    obj.number_of_timepoints,...
-    {obj.output_directory},...
-    obj.duration,...
-    obj.fundamental_period,...
-    'VariableNames',{'number_of_timepoints','output_directory','duration','fundamental_period'});
-writetable(SuperMDAtable,fullfile(obj.output_directory,'SuperMDA_config.txt'),'Delimiter','\t');
 %% Update the dependent parameters in the MDA object
 % Some parameters in the MDA object are dependent on others.
 % This dependency came about from combining parameters that are
@@ -50,3 +38,31 @@ for i = 1:obj.my_length
     obj.group(i).group_function_after_handle = str2func(obj.group(i).group_function_after_name);
 end
 obj.reflect_number_of_timepoints;
+%% Calculate the number of images
+% The number of images will be used to pre-allocate memory for the
+% database. Without memory pre-allocation the SuperMDA will grind to a
+% halt.
+image_counter = 0;
+for i = 1:obj.my_length
+    for j = 1:max(size(obj.group(i).position))
+        for k = 1:max(size(obj.group(i).position(j).settings))
+           image_counter = image_counter + sum(obj.group(i).position(j).settings.timepoints);
+        end
+    end
+end
+%% Pre-allocate the database
+%
+pre_allocation_cell = {'channel name','filename','group label','position label',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'image description'};
+pre_allocation_cell = repmat(pre_allocation_cell,image_counter,1);
+obj.database = cell2table(pre_allocation_cell);
+%%
+% Save the SuperMDA in a text or xml format, so that it can be reloaded
+% later on. SuperMDA is an object and objects don't necessarily load
+% properly, especially if listeners are involved.
+SuperMDAtable = table(...
+    obj.number_of_timepoints,...
+    {obj.output_directory},...
+    obj.duration,...
+    obj.fundamental_period,...
+    'VariableNames',{'number_of_timepoints','output_directory','duration','fundamental_period'});
+writetable(SuperMDAtable,fullfile(obj.output_directory,'SuperMDA_config.txt'),'Delimiter','\t');
