d78af692b169b39c7935dcb16807f168303ef987
karhohs-kishonyscope <gandalfisarockstar@gmail.com>
2014-03-18 13:30:42 -0400

    streamlining the core uM object

--- Core/Core_MicroManagerHandle.m
+++ Core/Core_MicroManagerHandle.m
@@ -14,64 +14,53 @@ classdef Core_MicroManagerHandle < handle
         ShutterDevice
         xyStageLimits
         zLimits
-        Timer_pos
-        Timer_pos_counter
-        Timer_pos_previous_pos
-        I
-        %% gui handles
-        %
-        gui_StageMap
     end
     properties (SetObservable)
+        I
         pos
     end
     methods
+%% The constructor
+% This function will fire when the object is created
         function obj = Core_MicroManagerHandle()
+            %%
+            % Load uM and assign general uM objects to obj properties
             hmsg = msgbox('Click ''OK'' after the micromanager configuration file has been loaded.','Wait For Configuration');
-            import org.micromanager.MMStudioMainFrame;
+            import org.micromanager.MMStudioMainFrame; 
             obj.gui = MMStudioMainFrame(false);
-            obj.gui.show;
-            uiwait(hmsg);
+            obj.gui.show; %this opens the uM gui that can be used at the same time MATLAB is open. Closing this gui closes MATLAB and vice versa.
+            uiwait(hmsg); %the uM gui must load the correct configuration and relys upon user input. MATLAB will wait until the user confirms that uM loaded properly.
             obj.core = obj.gui.getMMCore;
             obj.mda = obj.gui.getAcquisitionEngine();
-            
+            %%
+            % Change a few settings
             obj.core.enableStderrLog(0); %Log info sent to MATLAB command window is suppressed
             obj.core.enableDebugLog(0); %Debug info will not be saved to the log file
             obj.core.setProperty('Core', 'TimeoutMs', 19999); %The Nikon TI-e supposedly has an internal timeout of 20000ms.
-            my_comp_name = obj.core.getHostName.toCharArray';
-            if strcmp(my_comp_name,'LB89-6A-45FA')
-                %% Closet Scope
-                %
-                obj.xyStageDevice = obj.core.getXYStageDevice;
-                obj.core.setFocusDevice('TIZDrive'); %this is specific to the Nikon TI that we use
-                obj.FocusDevice = obj.core.getFocusDevice;
-                obj.core.setFocusDevice('TIPFSOffset'); %this is specific to the Nikon TI that we use
-                obj.AutoFocusDevice = obj.core.getFocusDevice;
-                obj.AutoFocusStatusDevice = obj.core.getAutoFocusDevice;
-            elseif strcmp(my_comp_name,'LB89-68-A06F')
-                %% Curtain Scope
-                %
-                obj.xyStageDevice = obj.core.getXYStageDevice;
-                obj.core.setFocusDevice('TIZDrive'); %this is specific to the Nikon TI that we use
-                obj.FocusDevice = obj.core.getFocusDevice;
-                obj.core.setFocusDevice('TIPFSOffset'); %this is specific to the Nikon TI that we use
-                obj.AutoFocusDevice = obj.core.getFocusDevice;
-                obj.AutoFocusStatusDevice = obj.core.getAutoFocusDevice;
-            elseif strcmp(my_comp_name,'KISHONYWAB111A')
-                %% Kishony Scope
-                %
-                obj.xyStageDevice = obj.core.getXYStageDevice;
-                obj.core.setFocusDevice('TIZDrive'); %this is specific to the Nikon TI that we use
-                obj.FocusDevice = obj.core.getFocusDevice;
-                obj.core.setFocusDevice('TIPFSOffset'); %this is specific to the Nikon TI that we use
-                obj.AutoFocusDevice = obj.core.getFocusDevice;
-                obj.AutoFocusStatusDevice = obj.core.getAutoFocusDevice;
-            else
-                obj.xyStageDevice = obj.core.getXYStageDevice;
-                obj.FocusDevice = obj.core.getFocusDevice;
-                obj.AutoFocusDevice = obj.core.getFocusDevice;
-                obj.AutoFocusStatusDevice = obj.core.getAutoFocusDevice;
-            end
+            %% Assign microscope specific devices to obj properties
+            % SuperMDA has been tested on the following systems:
+            % * Nikon Ti-e with PFS
+            %%
+            % The following devices are stored as object properties (tested
+            % device): 
+            %
+            % * xyStageDevice (Prior ProScan)
+            % * FocusDevice (Nikon TI Z-Drive)
+            % * AutoFocusDevice (Nikon PFS System)
+            % * AutoFocusStatusDevice (Another part of the Nikon PFS
+            % System)
+            % * CameraDevice (Hamamatsu Orca R2)
+            % * Channels (A reflection of the Channels group in uM)
+            % * ShutterDevice
+            % * xyStageLimits (predefined by the user)
+            % * zLimits (predefined by the user)
+            %
+            % Note that the stage limits are predefined by the user. A text
+            % file must be created containing this information. This can be
+            % created manually by moving the objective to its highest and
+            % lowest position and recording the values. The stage should
+            % have its values recorde at the upper-left and and lower-right
+            % corner.
             
             obj.CameraDevice = obj.core.getCameraDevice;
             
@@ -84,82 +73,76 @@ classdef Core_MicroManagerHandle < handle
             my_Channel = obj.core.getAvailableConfigs('Channel').toArray;
             obj.Channel = cell(my_Channel);
             obj.ShutterDevice = obj.core.getShutterDevice;
-            
+            %%
+            % We found some settings are specific to a device and these
+            % settings are assigned below to a given microscope using the
+            % computer hostname as a unique identifier. 
+            my_comp_name = obj.core.getHostName.toCharArray'; %the hostname is used as a unique identifier
             if strcmp(my_comp_name,'LB89-6A-45FA')
-                %% Closet Scope
-                %
+                %% 
+                % Closet Scope
+                obj.xyStageDevice = obj.core.getXYStageDevice;
+                obj.core.setFocusDevice('TIZDrive'); %this is specific to the Nikon TI that we use
+                obj.FocusDevice = obj.core.getFocusDevice;
+                obj.core.setFocusDevice('TIPFSOffset'); %this is specific to the Nikon TI that we use
+                obj.AutoFocusDevice = obj.core.getFocusDevice;
+                obj.AutoFocusStatusDevice = obj.core.getAutoFocusDevice;
                 obj.core.setProperty(obj.xyStageDevice,'TransposeMirrorX',1);
                 obj.core.setProperty(obj.xyStageDevice,'TransposeMirrorY',1);
                 [mfilepath,~,~] = fileparts(mfilename('fullpath'));
                 mytable = readtable(fullfile(mfilepath,'settings_LB89-6A-45FA.txt'));
                 obj.xyStageLimits = [mytable.xlim1,mytable.xlim2,mytable.ylim1,mytable.ylim2];
                 obj.zLimits = [mytable.zmin,mytable.zmax];
+                obj.core.setProperty(obj.xyStageDevice,'MaxSpeed',70); % 'MaxSpeed' range of [0,100].
             elseif strcmp(my_comp_name,'LB89-68-A06F')
-                %% Curtain Scope
-                % The curtain scope has a ProScan II with a 'MaxSpeed'
-                % range of [1,100];
-                %
+                %% 
+                % Curtain Scope
+                obj.xyStageDevice = obj.core.getXYStageDevice;
+                obj.core.setFocusDevice('TIZDrive'); %this is specific to the Nikon TI that we use
+                obj.FocusDevice = obj.core.getFocusDevice;
+                obj.core.setFocusDevice('TIPFSOffset'); %this is specific to the Nikon TI that we use
+                obj.AutoFocusDevice = obj.core.getFocusDevice;
+                obj.AutoFocusStatusDevice = obj.core.getAutoFocusDevice;
                 obj.core.setProperty(obj.xyStageDevice,'TransposeMirrorX',1);
                 obj.core.setProperty(obj.xyStageDevice,'TransposeMirrorY',1);
                 [mfilepath,~,~] = fileparts(mfilename('fullpath'));
                 mytable = readtable(fullfile(mfilepath,'settings_LB89-68-A06F.txt'));
                 obj.xyStageLimits = [mytable.xlim1,mytable.xlim2,mytable.ylim1,mytable.ylim2];
                 obj.zLimits = [mytable.zmin,mytable.zmax];
-                obj.core.setProperty(obj.xyStageDevice,'MaxSpeed',50);
+                obj.core.setProperty(obj.xyStageDevice,'MaxSpeed',50); %There was concern of slippage and slowing the scope down was thought to be a solution to prevent this. 'MaxSpeed' range of [0,100].
             elseif strcmp(my_comp_name,'KISHONYWAB111A')
-                %% Kishony Scope
-                % The Kishony scope has a ProScan III, which has a
-                % 'MaxSpeed' range of [0,100].
+                %% 
+                % Kishony Scope
+                obj.xyStageDevice = obj.core.getXYStageDevice;
+                obj.core.setFocusDevice('TIZDrive'); %this is specific to the Nikon TI that we use
+                obj.FocusDevice = obj.core.getFocusDevice;
+                obj.core.setFocusDevice('TIPFSOffset'); %this is specific to the Nikon TI that we use
+                obj.AutoFocusDevice = obj.core.getFocusDevice;
+                obj.AutoFocusStatusDevice = obj.core.getAutoFocusDevice;
                 obj.core.setProperty(obj.xyStageDevice,'TransposeMirrorX',1);
                 obj.core.setProperty(obj.xyStageDevice,'TransposeMirrorY',1);
                 [mfilepath,~,~] = fileparts(mfilename('fullpath'));
                 mytable = readtable(fullfile(mfilepath,'settings_LB89-68-A06F.txt'));
                 obj.xyStageLimits = [mytable.xlim1,mytable.xlim2,mytable.ylim1,mytable.ylim2];
                 obj.zLimits = [mytable.zmin,mytable.zmax];
-                obj.core.setProperty(obj.xyStageDevice,'MaxSpeed',70);
+                obj.core.setProperty(obj.xyStageDevice,'MaxSpeed',70); % 'MaxSpeed' range of [0,100].
+            else
+                obj.xyStageDevice = obj.core.getXYStageDevice;
+                obj.FocusDevice = obj.core.getFocusDevice;
+                obj.AutoFocusDevice = obj.core.getFocusDevice;
+                obj.AutoFocusStatusDevice = obj.core.getAutoFocusDevice;
             end
-            obj.Timer_pos_counter = 0;
-            obj.getXYZ;
-            obj.Timer_pos_previous_pos = obj.pos;
-            obj.Timer_pos = timer('ExecutionMode','fixedRate','Period',1,'TimerFcn',@(~,~) obj.Timer_pos_fun); %it seems to take approx. 0.2 seconds to read the position from the microscope. If a timer's callback takes longer to execute than the period MATLAB it will seem as if MATLAB is unresponsive from the command line.
-            start(obj.Timer_pos);
         end
-        %%
-        % Called just before the object is destroyed.
-        function delete(obj)
-            delete(obj.Timer_pos);
-        end
+%% Methods
+% These methods are meant to make life a little easier by having quick
+% calls to common activities
+% * getXYZ() = get the XYZ coordinates of the stage
+% * setXYZ() = move the stage to a desired position
+% * snapImage() = take a picture
         %% Get x, y, and z position of microscope
         %
-        function [my_pos] = getXYZ(obj)
-            Core_general_getXYZ(obj);
-            my_pos = obj.pos;
-            %%
-            % The two if-statements below are meant to be a fail safe
-            % should the stage not recognize that it has reached its
-            % limits. The Prior stage is smart enough to recognize when its
-            % limits are reached and power will quickly be shut off to the
-            % motors. However, should there be anything in the path of the
-            % objective the Prior stage will not recognize this and damage
-            % could occur. I'm not going to test this scenario, because I
-            % cannot think of a way to to do it without putting the
-            % microscope at risk. Instead, I wrote this note and I have my
-            % fingers crossed that should the following code ever be
-            % required that it will successfully stop the motor.
-            if (obj.core.deviceBusy(obj.xyStageDevice)) && (sum(abs(obj.pos-obj.Timer_pos_previous_pos))<50)
-                %%
-                % The stage may have reached one of its limits or is
-                % physically impeded by part of the microscope (Yikes!).
-                % This function assumes that the stage during normal
-                % operation should be able to move 50 um in second and if
-                % it is not then something is wrong.
-                obj.Timer_pos_counter = obj.Timer_pos_counter+1;
-            else
-                obj.Timer_pos_counter = 0;
-            end
-            if obj.Timer_pos_counter > 2
-                obj.core.stop(obj.xyStageDevice);
-            end
+        function [obj] = getXYZ(obj)
+            Core_method_getXYZ(obj);
         end
         %% Get x, y, and z position of microscope
         %
@@ -170,10 +153,12 @@ classdef Core_MicroManagerHandle < handle
             % following if statement unpacks the varargin, so that the
             % m-file sees the data as if this "middle-method" did not
             % exist.
+            %
+            % Note that setting the Z will turn off PFS.
             if isempty(varargin)
-                Core_general_setXYZ(obj,pos);
+                Core_method_setXYZ(obj,pos);
             else
-                Core_general_setXYZ(obj,pos,varargin{:});
+                Core_method_setXYZ(obj,pos,varargin{:});
             end
         end
         %% Get x, y, and z position of microscope
@@ -186,18 +171,12 @@ classdef Core_MicroManagerHandle < handle
             % m-file sees the data as if this "middle-method" did not
             % exist.
             if isempty(varargin)
-                Core_general_snapImage(obj);
+                Core_method_snapImage(obj);
             else
-                Core_general_snapImage(obj,varargin{:});
+                Core_method_snapImage(obj,varargin{:});
             end
         end
-        %%
-        %
-        function [obj] = Timer_pos_fun(obj)
-            Core_timer_Timer_pos_fun(obj);
-        end
     end
-    methods (Static)
-        
-    end
+%     methods (Static)
+%     end
 end
