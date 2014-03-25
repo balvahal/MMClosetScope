3111d38b5eaf5618865f7ee00587334704c91003
Kyle Wayne Karhohs <gandalfisarockstar@gmail.com>
2014-03-25 11:20:24 -0400

    stripping functions from SuperMDA

--- SuperMDA/SuperMDA.m
+++ SuperMDA/SuperMDA.m
@@ -1,48 +1,49 @@
 %% The SuperMDA
-% The SuperMDA allows multiple multi-dimensional-acquisitions to be
-% run simulataneously. Each group consists of 1 or more positions. Each
+% The SuperMDA allows multiple multi-dimensional-acquisitions to be run
+% simulataneously. Each group consists of 1 or more positions. Each
 % position consists of 1 or more settings.
 %
-% One thing I have struggled with is redundancy in the SuperMDA. In the most common
-% cases where no feedback is implemented much of the data stored in the
-% SuperMDA is redundant. For example, if 1000 timepoints are taken at the
-% same exposure should I keep an array of size 1000 that stores just that
-% single number? Such an array might be termed _sparse_, because the
-% information in the array is near nil. However, having such an array is
-% convenient from a programming sense, because it is easy to iterate over
-% without the need to parse through a set of rules that determine what will
-% happen at a given timepoint; perhaps a rules-based MDA would be more
-% compact, but its interpretation not as clear as a multi-dimensional lookup table. 
-% From a performance standpoint it is necessary to pre-allocate enough
-% memory for the SuperMDA, but without going too far and running into the
-% limits of memory in terms of size and speed, which are surprisingly easy
-% to reach due to the
-% combinatoric explosion of the number of settings.
+% One thing I have struggled with is redundancy in the SuperMDA. In the
+% most common cases where no feedback is implemented much of the data
+% stored in the SuperMDA is redundant. For example, if 1000 timepoints are
+% taken at the same exposure should I keep an array of size 1000 that
+% stores just that single number? Such an array might be termed _sparse_,
+% because the information in the array is near nil. However, having such an
+% array is convenient from a programming sense, because it is easy to
+% iterate over without the need to parse through a set of rules that
+% determine what will happen at a given timepoint; perhaps a rules-based
+% MDA would be more compact, but its interpretation not as clear as a
+% multi-dimensional lookup table. From a performance standpoint it is
+% necessary to pre-allocate enough memory for the SuperMDA, but without
+% going too far and running into the limits of memory in terms of size and
+% speed, which are surprisingly easy to reach due to the combinatoric
+% explosion of the number of settings.
 %
-% Regardless, At some point the
-% information in the SuperMDA will become _full_ in a sense, because
-% each picture that is captured and stored is saved with some metadata.
-% The goal of the SuperMDA is on some level just creating all the
-% metadata in advance, which represents a plan of action for the collection
-% of images; so what's the harm in storing everything in centralized arrays if all this
-% information will eventually be stored in a distributed manner anyways?
+% Regardless, At some point the information in the SuperMDA will become
+% _full_ in a sense, because each picture that is captured and stored is
+% saved with some metadata. The goal of the SuperMDA is on some level just
+% creating all the metadata in advance, which represents a plan of action
+% for the collection of images; so what's the harm in storing everything in
+% centralized arrays if all this information will eventually be stored in a
+% distributed manner anyways?
 %
 % Perhaps my frustraion is moot, because there is a limit to the number of
 % images that can be captured in any given experiment based on the time it
 % takes to acquire the images and the sensitivity of living cells to light.
 % Also, this doesn't have to be the perfect system, it just has to work.
 % Therefore, the array scheme will be kept even if it will be under
-% utilized and wasteful; it is easy to implement and understand. The number of groups, positions, timepoints, and settings will
-% change based on the nature of each experiment, but the maximum number of
-% images acquired for each experiment will be roughly the same. For
-% example. Scanning large surfaces will yield either a few groups with many
-% positions in the case of slides, or many groups with few positions in the
-% case of multi-well plates. There is a natural tradeoff between the
-% frequency of imaging and the number of positions.
+% utilized and wasteful; it is easy to implement and understand. The number
+% of groups, positions, timepoints, and settings will change based on the
+% nature of each experiment, but the maximum number of images acquired for
+% each experiment will be roughly the same. For example. Scanning large
+% surfaces will yield either a few groups with many positions in the case
+% of slides, or many groups with few positions in the case of multi-well
+% plates. There is a natural tradeoff between the frequency of imaging and
+% the number of positions.
 %
-% In conclusion, there is a limit to the SuperMDA based upon its design where
-% there are no rules and each where, what, and when has an explicit answer
-% stored in memory (though the answers can be modified on the fly).
+% In conclusion, there is a limit to the SuperMDA based upon its design
+% where there are no rules and each where, what, and when has an explicit
+% answer stored in memory (though the answers can be modified on the fly).
 % SuperMDA would be not be appropriate for experiments that have an
 % indefinite length of time involved. SuperMDA is |for loop| as opposed to
 % a |while loop|. Memory concerns should not become apparent, because only
@@ -52,12 +53,12 @@
 % mind:
 %
 % * (groups,positions,settings,timepoints)
-% * 384well plates for a movie: (384,8,6,192); approx 4TB of image data; the plate
-% could be imaged twice an hour for 4 days.
-% * 96well plates for a movie: (96,32,6,192); approx 4TB of image data; the plate
-% could be imaged twice an hour for 4 days.
-% * 24well plates for a movie: (24,128,6,192); approx 4TB of image data; the plate
-% could be imaged twice an hour for 4 days.
+% * 384well plates for a movie: (384,8,6,192); approx 4TB of image data;
+% the plate could be imaged twice an hour for 4 days.
+% * 96well plates for a movie: (96,32,6,192); approx 4TB of image data; the
+% plate could be imaged twice an hour for 4 days.
+% * 24well plates for a movie: (24,128,6,192); approx 4TB of image data;
+% the plate could be imaged twice an hour for 4 days.
 % * 6x  dishes for a movie: (6,64,6,576); approx 2TB of image data; the
 % plates can be imaged every 10 minutes for 4 days.
 %
@@ -81,7 +82,7 @@ classdef SuperMDA < handle
         duration;
         fundamental_period; %The units are seconds.
         group;
-        group_order = 1;
+        group_order;
         mda_clock_absolute;
         mda_clock_pointer = 1;
         mda_clock_relative = 0;
@@ -98,7 +99,7 @@ classdef SuperMDA < handle
         settings;
     end
     properties (SetObservable)
-
+        
     end
     %%
     %
@@ -134,9 +135,9 @@ classdef SuperMDA < handle
                 obj.prototype_settings.gain = 1;
                 obj.prototype_settings.settings_function_name = 'SuperMDA_function_settings_basic';
                 obj.prototype_settings.settings_function_handle = str2func(obj.prototype_settings.settings_function_name);
-                obj.prototype_settings.exposure = 0;
+                obj.prototype_settings.exposure = 1; %This is a customizable arrray
                 obj.prototype_settings.period_multiplier = 1;
-                obj.prototype_settings.timepoints = 1;
+                obj.prototype_settings.timepoints = 1; %This is a customizable array
                 obj.prototype_settings.user_data = [];
                 obj.prototype_settings.z_origin_offset = 0;
                 obj.prototype_settings.z_stack = 0;
@@ -154,7 +155,7 @@ classdef SuperMDA < handle
                 obj.prototype_position.position_function_before_handle = str2func(obj.prototype_settings.position_function_before_name);
                 obj.prototype_position.settings_order = 1;
                 obj.prototype_position.user_data = [];
-                obj.prototype_position.xyz = obj.mm.getXYZ;
+                obj.prototype_position.xyz = obj.mm.getXYZ; %This is a customizable array
                 %% initialize the prototype_group
                 %
                 obj.prototype_group.label = '';
@@ -162,166 +163,49 @@ classdef SuperMDA < handle
                 obj.prototype_position.group_function_after_handle = str2func(obj.prototype_settings.group_function_after_name);
                 obj.prototype_position.group_function_before_name = 'SuperMDA_function_group_before_basic';
                 obj.prototype_position.group_function_before_handle = str2func(obj.prototype_settings.group_function_before_name);
-                obj.prototype_group.position_order = 1; 
+                obj.prototype_group.position_order = 1;
                 obj.prototype_group.user_data = [];
-
-            elseif nargin == 2
-                %%
-                % The _filename_ means that a SuperMDA will be loaded.
-            end
-        end
-        %% Copy
-        %
-        % Make a copy of a handle object.
-        function new = copy(obj)
-            % Instantiate new object of the same class.
-            new = feval(class(obj));
-            
-            % Copy all non-hidden properties.
-            p = properties(obj);
-            for i = 1:length(p)
-                if strcmp('group',p{i})
-                    for j=1:length(obj.(p{i}))
-                        if j==1
-                            new.(p{i}) = obj.(p{i})(j).copy;
-                        else
-                            new.(p{i})(j) = obj.(p{i})(j).copy;
-                        end
-                    end
-                else
-                    new.(p{i}) = obj.(p{i});
-                end
             end
         end
         %% preallocate memory to hold the SuperMDA information
-        % This should always be done before 
-        function obj = preAllocateMemory(obj, myDuration, myFundamental_period, myNumberOfGroups, myNumberOfPositions, myNumberOfSettings)
+        % This should always be done before and the largest number should
+        % be used for the number of groups, positions, and settings
+        function obj = preAllocateMemoryAndInitialize(obj, myDuration, myFundamental_period, myNumberOfGroups, myNumberOfPositions, myNumberOfSettings)
             p = inputParser;
             addRequired(p, 'obj', @(x) isa(x,'SuperMDA'));
-            addRequired(p, 'myDuration', isnumeric(x));
-            addRequired(p, 'myFundamental_period', isnumeric(x));
-            addRequired(p, 'myNumberOfGroups', isinteger(x));
-            addRequired(p, 'myNumberOfPositions', isinteger(x));
-            addRequired(p, 'myNumberOfSettings', isinteger(x));
+            addRequired(p, 'myDuration', @(x) isnumeric(x) && (x>0)); %in seconds
+            addRequired(p, 'myFundamental_period', @(x) isnumeric(x) && (x>0)); %in seconds
+            addRequired(p, 'myNumberOfGroups', @(x) isinteger(x) && (x>0));
+            addRequired(p, 'myNumberOfPositions', @(x) isinteger(x) && (x>0));
+            addRequired(p, 'myNumberOfSettings', @(x) isinteger(x) && (x>0));
             parse(p, myDuration, myFundamental_period, myNumberOfGroups, myNumberOfPositions, myNumberOfSettings);
             %% Calculate the number of timepoints
-            %
-            obj.number_of_timepoints = floor(p.myDuration/p.myFundamental_period);
-            %% Update prototypes
-            %
-
-                [obj.settings{:}] = deal(obj.prototype_settings);
-                [obj.position{:}] = deal(obj.prototype_position);
-                [obj.group{:}] = deal(obj.prototype_group);
-        end
-        %% clone
-        %
-        function obj = clone(obj,obj2)
-            % Make sure objects are of the same type
-            if class(obj) == class(obj2)
-                % Copy all non-hidden properties.
-                p = properties(obj);
-                for i = 1:length(p)
-                    if strcmp('group',p{i})
-                        obj.(p{i}) = [];
-                        for j=1:length(obj.(p{i}))
-                            if j==1
-                                obj.(p{i}) = obj2.(p{i})(j).copy;
-                            else
-                                obj.(p{i})(j) = obj2.(p{i})(j).copy;
-                            end
-                        end
-                    else
-                        obj.(p{i}) = obj2.(p{i});
-                    end
-                end
-            end
-        end
-        %% create a new group
-        %
-        function obj = new_group(obj)
-            %first, borrow the properties from the last group to provide a
-            %starting point and make sure the parent object is consistent
-            obj.group(end+1) = obj.group(end).copy;
-            obj.group_order(end+1) = obj.my_length;
-            obj.group(end).label = '';
-        end
-        %% Find the number of group objects.
-        %
-        function len = my_length(obj)
-            obj_array = obj.group;
-            len = length(obj_array);
-        end
-        %% change the same property for all group
-        %
-        function obj = change_all_group(obj,my_property_name,my_var)
-            switch(lower(my_property_name))
-                case 'travel_offset'
-                    if isnumeric(my_var) && length(my_var) == 1
-                        for i=1:obj.my_length
-                            obj.group(i).travel_offset = my_var;
-                        end
-                    end
-                case 'travel_offset_bool'
-                    if islogical(my_var) && length(my_var) == 1
-                        for i=1:obj.my_length
-                            obj.group(i).travel_offset_bool = my_var;
-                        end
-                    end
-                case 'group_function_after_name'
-                    if ischar(my_var)
-                        for i=1:obj.my_length
-                            obj.group(i).group_function_after_name = my_var;
-                        end
-                    end
-                case 'group_function_before_name'
-                    if ischar(my_var)
-                        for i=1:obj.my_length
-                            obj.group(i).group_function_before_name = my_var;
-                        end
-                    end
-                case 'parent_mdaprimary'
-                    %This really shouldn't ever need to be called, because
-                    %by definition every child shares the same parent
-                    if isa(my_var,'SuperMDALevel2Group')
-                        for i=1:max(size(obj.position))
-                            obj.group(i).Parent_MDAGroup = my_var;
-                        end
-                    end
-                case 'position'
-                    if isa(my_var,'SuperMDALevel3Position')
-                        for i=1:obj.my_length
-                            obj.group(i).position = [];
-                            for j=1:length(my_var)
-                                if j == 1
-                                    obj.group(i).position = my_var(j).copy;
-                                else
-                                    obj.group(i).position(j) = my_var(j).copy;
-                                end
-                            end
-                        end
-                    end
-                case 'position_order'
-                    if isnumeric(my_var)
-                        for i=1:obj.my_length
-                            obj.group(i).position_order = my_var;
-                        end
-                    end
-                case 'label'
-                    if ischar(my_var)
-                        for i=1:obj.my_length
-                            obj.group(i).label = my_var;
-                        end
-                    end
-                otherwise
-                    warning('primary:chg_all','The property entered was not recognized');
-            end
-        end
-        %% Configure the relative clock
-        %
-        function obj = configure_clock_relative(obj)
+            % and check that duration and the fundamental period are
+            % consistent.
+            obj.fundamental_period = p.myFundamental_period;
+            obj.number_of_timepoints = floor(p.myDuration/obj.fundamental_period)+1; %ensures fundamental period and duration are consistent with each other
+            obj.duration = obj.fundamental_period*(obj.number_of_timepoints-1);
             obj.mda_clock_relative = 0:obj.fundamental_period:obj.duration;
-            obj.number_of_timepoints = length(obj.mda_clock_relative);
+            %% Update prototypes
+            % * settings: exposure and timepoints
+            % * position: xyz
+            obj.prototype_settings.exposure = ones(obj.number_of_timepoints,1);
+            obj.prototype_settings.timepoints = ones(obj.number_of_timepoints,1);
+            obj.mm.getXYZ;
+            obj.prototype_position.xyz = ones(obj.number_of_timepoints,3);
+            obj.prototype_position.xyz(:,1) = obj.mm.pos(1);
+            obj.prototype_position.xyz(:,2) = obj.mm.pos(2);
+            obj.prototype_position.xyz(:,3) = obj.mm.pos(3);
+            %%
+            % set the order variables
+            obj.prototype_position.settings_order = 1:myNumberOfSettings;
+            obj.prototype_group.position_order = 1:myNumberOfPositions;
+            obj.group_order = 1:myNumberOfGroups;
+            %% Fill the SuperMDA with this preallocated information
+            %
+            [obj.settings{:}] = deal(obj.prototype_settings);
+            [obj.position{:}] = deal(obj.prototype_position);
+            [obj.group{:}] = deal(obj.prototype_group);
         end
         %% Configure the absolute clock
         % Convert the MDA object unit of time (seconds) to the MATLAB unit
@@ -393,16 +277,6 @@ classdef SuperMDA < handle
     %%
     %
     methods (Static)
-        %% update array length of customizables
-        % The customizables are xyz, exposure, and timepoints
-        function updateCustomizables(~,evtdata)
-            evtdata.AffectedObject.reflect_number_of_timepoints;
-            switch evtdata.Source.Name
-                case 'duration'
-                    evtdata.AffectedObject.duration = evtdata.AffectedObject.mda_clock_relative(end);
-                case 'fundamental_period'
-                    evtdata.AffectedObject.duration = evtdata.AffectedObject.mda_clock_relative(end);
-            end
-        end
+        
     end
 end
