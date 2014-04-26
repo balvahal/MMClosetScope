c12035ee702331e39f4dc0af15e7905f154705aa
karhohs-kishonyscope <gandalfisarockstar@gmail.com>
2014-04-26 19:27:23 -0400

    estimates upper limit of size. Group doesn't disable PFS

--- SuperMDA/SuperMDA_function_group_before_basic.m
+++ SuperMDA/SuperMDA_function_group_before_basic.m
@@ -11,7 +11,7 @@ k = smdaPilot.runtime_index(4); %settings
 %% lower objective
 mmhandle.getXYZ;
 pos_next = mmhandle.pos + [0,0,-100];
-mmhandle.setXYZ(pos_next(3),'direction','z');
+mmhandle.setXYZ(1000,'direction','z');
 smdaPilot.mm.core.waitForDevice(smdaPilot.mm.FocusDevice);
 %% find and travel to next position
 pos_next = smdaPilot.itinerary.group(i).position(1).xyz(t,:);
@@ -19,6 +19,14 @@ mmhandle.setXYZ(pos_next(1:2));
 mmhandle.core.waitForDevice(mmhandle.xyStageDevice);
 mmhandle.setXYZ(pos_next(3),'direction','z');
 smdaPilot.mm.core.waitForDevice(smdaPilot.mm.FocusDevice);
-%     smdaPilot.mm.core.setProperty(smdaPilot.mm.AutoFocusDevice,'Position',smdaPilot.itinerary.group(i).position(1).continuous_focus_offset);
-%     smdaPilot.mm.core.setProperty(smdaPilot.mm.AutoFocusStatusDevice,'State','On')
-%     pause(5);
+%% if using perfect focus...
+% PFS will turn off if the z-drive is set. Therefore it must be turned back
+% on. Assume PFS is being utilized if the first position uses PFS. PFS will
+% be utilized even in mixed cases, but then fullFocus() will turn the PFS
+% on and off at every position that uses PFS, which can slow things down
+% and make a lot of beeping sounds.
+if smdaPilot.itinerary.group(i).position(1).continuous_focus_bool
+    smdaPilot.mm.core.setProperty(smdaPilot.mm.AutoFocusDevice,'Position',smdaPilot.itinerary.group(i).position(1).continuous_focus_offset);
+    smdaPilot.mm.core.setProperty(smdaPilot.mm.AutoFocusStatusDevice,'State','On');
+    pause(3);
+end
--- SuperMDA/SuperMDA_method_start_acquisition.m
+++ SuperMDA/SuperMDA_method_start_acquisition.m
@@ -1,30 +1,38 @@
 %%
 %
-function [obj] = SuperMDA_method_start_acquisition(obj)
+function [smdaPilot] = SuperMDA_method_start_acquisition(smdaPilot)
 %%
 %
-if ~obj.running_bool
-    obj.itinerary.finalize_MDA;
+if ~smdaPilot.running_bool
+    smdaPilot.itinerary.finalize_MDA;
     %% Estimate the total memory requirement
     % and verify enough room is on the hard drive (assuming the largest
     % images will be taken)
-    obj.mm.core.setProperty(smdaPilot.mm.CameraDevice,'Binning','1x1');
+    smdaPilot.mm.core.setProperty(smdaPilot.mm.CameraDevice,'Binning','1x1');
     sizeOfBinaryImage = smdaPilot.mm.core.getImageWidth*smdaPilot.mm.core.getImageHeight*16/8; %16 for 16 bit image and 8 for 8 bits in a byte
-    sizeOfAllImages = sizeOfBinaryImage*obj.itinerary.total_number_images; %in bytes
-    disp(sizeOfAllImages);
-    waitforbuttonpress;
+    sizeOfAllImages = sizeOfBinaryImage*smdaPilot.itinerary.total_number_images/1024/1024; %in megabytes
+    str = sprintf('Images may take up to %0.6g MB of disk spaces.\nDo you want to proceed?',sizeOfAllImages);
+    choice = questdlg(str,'Continue','Yes','No','No');
+    switch choice
+        case 'No'
+            return
+        case 'Yes'
+            disp('all systems go');
+        otherwise
+            return
+    end
     %%
     %
-    obj.runtime_index = [1,1,1,1,1];
-    obj.mda_clock_pointer = 1;
+    smdaPilot.runtime_index = [1,1,1,1,1];
+    smdaPilot.mda_clock_pointer = 1;
     %% Configure the absolute clock
-    % Convert the MDA object unit of time (seconds) to the MATLAB unit of time
+    % Convert the MDA smdaPilotect unit of time (seconds) to the MATLAB unit of time
     % (days) for the serial date numbers, i.e. the number of days that have
     % passed since January 1, 0000.
-    obj.mda_clock_absolute = now + obj.itinerary.mda_clock_relative/86400;
-    obj.runtime_timer.StopFcn = {@SuperMDA_function_runtime_timer_stopfcn,obj};
-    obj.running_bool = true;
-    start(obj.wait_timer);
-    obj.runtime_timer.StartDelay = 0; %this value must be reset after stopping it b/c it holds time left on timer.
-    start(obj.runtime_timer);
+    smdaPilot.mda_clock_absolute = now + smdaPilot.itinerary.mda_clock_relative/86400;
+    smdaPilot.runtime_timer.StopFcn = {@SuperMDA_function_runtime_timer_stopfcn,smdaPilot};
+    smdaPilot.running_bool = true;
+    start(smdaPilot.wait_timer);
+    smdaPilot.runtime_timer.StartDelay = 0; %this value must be reset after stopping it b/c it holds time left on timer.
+    start(smdaPilot.runtime_timer);
 end
