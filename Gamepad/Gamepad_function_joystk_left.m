4397fe02952629bd6c10c749571ff756b814fd50
karhohs <gandalfisarockstar@gmail.com>
2014-07-15 15:40:53 -0400

    joystick power!

--- Gamepad/Gamepad_function_joystk_left.m
+++ Gamepad/Gamepad_function_joystk_left.m
@@ -7,23 +7,25 @@ end
 
 my_mag = gamepad.joystk_left_mag - 0.1;
 if my_mag <= 0
-    my_command = sprintf('JXD,0\r'); %stop X movement
-    gamepad.microscope.core.writeToSerialPort(gamepad.microscope.xyStageDevice,my_command);
-    my_command = sprintf('JYD,0\r'); %stop Y movement
-    gamepad.microscope.core.writeToSerialPort(gamepad.microscope.xyStageDevice,my_command);
+    % stop the joystick movement
+    gamepad.microscope.core.setSerialPortCommand('COM3','VS,0,0',sprintf('\r'));
+    %e.g.
+    %gamepad.microscope.core.setSerialPortCommand('COM3','GR,1000,1000',sprintf('\r'));
+    % after setting JXD to 0, read back the answer with...
+    % gamepad.microscope.core.getSerialPortAnswer('COM3',sprintf('\r'));
+    % send a query request and then getSerialPortAnswer: gamepad.microscope.core.setSerialPortCommand('COM3','O',sprintf('\r'));
     return;
 end
 %%
 % The 2nd order polynomial has a nice gradual increase in speed for small
 % magnitudes that gives the joystick movement some fine touch.
-my_speed = round(99*(my_mag^2))+1; %a percentage between 1 and 100 %speed of scope [1,250] with default = 100. acceleration of scope [1,150] with default = 100;
+% the manual recommends setting the joystick between -8000 and 8000 microns
+% per second, though the max speed is [-30000,30000]
+my_speed = round(15000*(my_mag^2)); %a percentage between 1 and 100 %speed of scope [1,250] with default = 100. acceleration of scope [1,150] with default = 100;
 
 my_dir = gamepad.joystk_left_dir + 180; %the default of the joystick has 0 degrees pointing East and degrees increase clockwise. For the scope we want 0 degrees pointing West increasing clockwise.
 %gamepad.microscope.core.stop(gamepad.microscope.xyStageDevice);
-my_command = sprintf('JXD,%d\r',cosd(my_dir)); % X movement
-gamepad.microscope.core.writeToSerialPort(gamepad.microscope.xyStageDevice,my_command);
-my_command = sprintf('JYD,%d\r',sind(my_dir)); % Y movement
-gamepad.microscope.core.writeToSerialPort(gamepad.microscope.xyStageDevice,my_command);
-my_command = sprintf('O,%d\r',my_speed); % speed as percentage of current speed setting
-gamepad.microscope.core.writeToSerialPort(gamepad.microscope.xyStageDevice,my_command);
+my_command = sprintf('VS,%d,%d',my_speed*cosd(my_dir),my_speed*sind(my_dir));
+gamepad.microscope.core.setSerialPortCommand('COM3',my_command,sprintf('\r'));
+fprintf('%3.2d   %3.2d \r\n',my_mag,my_dir);
 end
