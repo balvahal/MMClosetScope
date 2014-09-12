a28c65b1b0e5d33e169e04081c7de0b47c8e6de6
Kyle Wayne Karhohs <gandalfisarockstar@gmail.com>
2014-09-12 15:43:17 -0400

    more readme for grid maker and readability edits

--- SuperMDA/SuperMDA_grid_maker.m
+++ SuperMDA/SuperMDA_grid_maker.m
@@ -39,6 +39,24 @@
 %
 % NOI means number of images. ULC means upper lefthand corner. LRC means
 % lower righthand corner.
+%
+% What is meant by ULC and LRC and the centroid? The ULC and LRC
+% are positions on the stage and do not represent any particular
+% pixel in an image. The position could represent any particular
+% pixel in the image, so I like to imagine it as the upper-left
+% corner pixel or the traditional first pixel in an image. When
+% making a grid of images this means that the ULC will be the same
+% as the first pixel in the first image, but the LRC will be the
+% last pixel of the area covered by the grid offset by the length
+% and height of a single image. The centroid could be the actual
+% pixel that is the center of a grid or the first pixel of the
+% image that is deemed to be the center image. This is the
+% challenge of keeping track of regions with area using a point.
+% However, to keep the convention of the first pixel in an image
+% being represented by the stage position then the centroid shall
+% represent the first pixel as well. This will make the math
+% consistent. When images are abstracted as points the math will be
+% performed with reference to the first pixel of each image.
 function [grid] = SuperMDA_grid_maker(mmhandle,varargin)
 %% Parse the input
 % A grid of images can be defined by many combinations of its
@@ -71,7 +89,8 @@ parse(p,mmhandle,varargin{:});
 % value chosen for _overlap_ parameter is used.
 %
 % If the units are micro-meters then a conversion is made. _overlap_x_ and
-% _overlap_y_ are also assessed.
+% _overlap_y_ are defined. The code ultimately depends on _overlap_x_ and
+% _overlap_y_, not _overlap_.
 if strcmp(p.Results.overlap_units,'um') %if 1
     overlap = round(p.Results.overlap/mmhandle.core.getPixelSizeUm);
     if strcmp(p.Results.overlap_x,'undefined') %if 1_1
@@ -152,15 +171,15 @@ if (~strcmp(p.Results.upper_left_corner,'undefined')) && (~strcmp(p.Results.lowe
 end
 %% Parse valid combinations of grid characteristics
 % The grid to be made depends upon the collection of parameters that were
-% specified when the function was called. The parameters that
-% default to the string 'undefined' determine the type of grid to be made.
-% Like a barcode, a logical array is created, identifying which of the
-% parameters were left undefined 
-% with a logical 1.
+% specified when the function was called. The parameters that default to
+% the string 'undefined' determine the type of grid to be made. Like a
+% barcode, a logical array is created, identifying which of the parameters
+% were left undefined with a logical 1. User-specified parameters are
+% logical 0.
 %
 % The parameters that do not default to 'undefined' are not part of the
-% decision array. After the decision array is defined it is
-% converted into a base 10 number.
+% decision array. After the decision array is defined it is converted into
+% a base 10 number.
 barcodeParameters = {'number_of_images','number_of_columns','number_of_rows','centroid','upper_left_corner','lower_right_corner'};
 decision_array = ones(1,numel(barcodeParameters));
 for i = 1:(numel(barcodeParameters))
@@ -176,8 +195,10 @@ decision_number = bin2dec(int2str(decision_array));
 % * im_num_col: number of columns
 % * im_num_row: number of rows
 % * NOI: number of images total
-% * overlap_x: pixel overlap between images in the x direction
-% * overlap_y: pixel overlap between images in the y direction
+% * overlap_x: pixel overlap between images in the x direction (and already
+% defined above).
+% * overlap_y: pixel overlap between images in the y direction (and already
+% defined above).
 % * pixHeight: image height in pixels
 % * pixWidth: image width in pixels
 % * ULC: the upper lefthand corner of the grid.
@@ -187,6 +208,10 @@ decision_number = bin2dec(int2str(decision_array));
 % will determine the correct code to execute in order to "fill in any
 % blanks" in the list of variables above.
 %%%
+% Valid Parameter combinations
+%
+% #
+%
 % The uManager core object can be queried for the height and width of the
 % image, but for code readability these values are stored in separate
 % variables and are necessary for defining the image grid.
@@ -199,45 +224,12 @@ switch decision_number
     % The _centroid_ and _number_of_images_ was defined by the user. In
     % this case the shape of the grid will be automatically determined.
     case 36 %centroid + number_of_images
-        %% Specify upper-left and lower-right corners
-        %
-        % The rectangular shape of the image will be automatically
-        % generated. The number of images may not match the input number of
-        % images.
-        
-        widthCandidates = floor(sqrt(p.Results.number_of_images./(linspace(0.5625,1,10)*pixWidth/pixHeight)));
-        objectiveArray = mod(p.Results.number_of_images,widthCandidates);
-        [~,ind] = min(objectiveArray);
-        im_num_col = widthCandidates(ind);
-        im_num_row = floor(p.Results.number_of_images/im_num_col);
-        NOI = im_num_col*im_num_row;
-        while NOI < p.Results.number_of_images
-            if im_num_col < im_num_row
-                im_num_col = im_num_col + 1;
-            else
-                im_num_row = im_num_row + 1;
-            end
-            NOI = im_num_col*im_num_row;
-        end
-        
-        %% update the coordinates for the upper-left and lower-right
-        % What is meant by ULC and LRC and the centroid? The ULC and LRC
-        % are positions on the stage and do not represent any particular
-        % pixel in an image. The position could represent any particular
-        % pixel in the image, so I like to imagine it as the upper-left
-        % corner pixel or the traditional first pixel in an image. When
-        % making a grid of images this means that the ULC will be the same
-        % as the first pixel in the first image, but the LRC will be the
-        % last pixel of the area covered by the grid offset by the length
-        % and height of a single image. The centroid could be the actual
-        % pixel that is the center of a grid or the first pixel of the
-        % image that is deemed to be the center image. This is the
-        % challenge of keeping track of regions with area using a point.
-        % However, to keep the convention of the first pixel in an image
-        % being represented by the stage position then the centroid shall
-        % represent the first pixel as well. This will make the math
-        % consistent. When images are abstracted as points the math will be
-        % performed with reference to the first pixel of each image.
+        %%% 
+        % Specify upper-left and lower-right corners. The minRatio has been
+        % given a value of 9/16 or 0.5625. The maxRatio is 1 o
+        [NOI,im_num_row,im_num_col] = findGridSize(p.Results.number_of_images,pixWidth,pixHeight,overlap_x,overlap_y);
+        %%%
+        % update the coordinates for the upper-left and lower-right
         ULC = ...
             [p.Results.centroid(1) - (im_num_col-1)/2*(pixWidth-overlap_x)*mmhandle.core.getPixelSizeUm,...
             p.Results.centroid(2) - (im_num_row-1)/2*(pixHeight-overlap_y)*mmhandle.core.getPixelSizeUm,...
@@ -246,13 +238,19 @@ switch decision_number
             [p.Results.centroid(1) + (im_num_col-1)/2*(pixWidth-overlap_x)*mmhandle.core.getPixelSizeUm,...
             p.Results.centroid(2) + (im_num_row-1)/2*(pixHeight-overlap_y)*mmhandle.core.getPixelSizeUm,...
             p.Results.centroid(3)];
+        %% _upper_left_corner_, _lower_right_corner_, and _number of images_
+        %
     case 35 %upper-left, lower-right, and number of images
         % Use when you want a given number of images to fill a given space.
         % This means the overlap (or underlap?) between images must be
         % calculated
-        a = lower_right_corner(1) - upper_left_corner(1);
-        b = lower_right_corner(2) - upper_left_corner(2);
-        im_num_col = sqrt(p.Results.number_of_images*a/b)*pixHeight/pixWidth;
+        case35areaWidth = lower_right_corner(1) - upper_left_corner(1);
+        case35areaHeight = lower_right_corner(2) - upper_left_corner(2);
+        case35ImageInWidth = case35areaWidth/pixWidth;
+        case35ImageInHeight = case35areaHeight/pixHeight;
+        case35NOIInArea = case35ImageInWidth*case35ImageInHeight;
+        case35ColumnReductionFactor = sqrt(case35NOIInArea/p.Results.number_of_images)*pixHeight/pixWidth;
+        im_num_col = case35ImageInWidth/case35ColumnReductionFactor;
         im_num_row = p.Results.number_of_images/im_num_col;
         im_num_col = round(im_num_col);
         im_num_row = round(im_num_row);
@@ -265,7 +263,9 @@ switch decision_number
             end
             NOI = im_num_col*im_num_row;
         end
-        %%
+        %%%
+        % The overlap between images in the x and y direction are
+        % determined.
         overlap_x = -(lower_right_corner(1) - upper_left_corner(1))/mmhandle.core.getPixelSizeUm/(im_num_col-1)+pixWidth;
         overlap_y = -(lower_right_corner(2) - upper_left_corner(2))/mmhandle.core.getPixelSizeUm/(im_num_row-1)+pixHeight;
         ULC = upper_left_corner;
@@ -274,40 +274,14 @@ switch decision_number
             ULC(2)+(im_num_row-1)*(pixHeight-overlap_y)*mmhandle.core.getPixelSizeUm,...
             lower_right_corner(3)];
     case 34 %upper-left and number of images
-        widthCandidates = floor(sqrt(p.Results.number_of_images./(linspace(0.5625,1,10)*pixWidth/pixHeight)));
-        objectiveArray = mod(p.Results.number_of_images,widthCandidates);
-        [~,ind] = min(objectiveArray);
-        im_num_col = widthCandidates(ind);
-        im_num_row = floor(p.Results.number_of_images/im_num_col);
-        NOI = im_num_col*im_num_row;
-        while NOI < p.Results.number_of_images
-            if im_num_col < im_num_row
-                im_num_col = im_num_col + 1;
-            else
-                im_num_row = im_num_row + 1;
-            end
-            NOI = im_num_col*im_num_row;
-        end
+        [NOI,im_num_row,im_num_col] = findGridSize(p.Results.number_of_images,pixWidth,pixHeight,overlap_x,overlap_y);
         ULC = upper_left_corner;
         LRC = ...
             [ULC(1) + (im_num_col-1)*(pixWidth-overlap_x)*mmhandle.core.getPixelSizeUm,...
             ULC(2) + (im_num_row-1)*(pixHeight-overlap_y)*mmhandle.core.getPixelSizeUm,...
             ULC(3)];
     case 33 %lower-right and number of images
-        widthCandidates = floor(sqrt(p.Results.number_of_images./(linspace(0.5625,1,10)*pixWidth/pixHeight)));
-        objectiveArray = mod(p.Results.number_of_images,widthCandidates);
-        [~,ind] = min(objectiveArray);
-        im_num_col = widthCandidates(ind);
-        im_num_row = floor(p.Results.number_of_images/im_num_col);
-        NOI = im_num_col*im_num_row;
-        while NOI < p.Results.number_of_images
-            if im_num_col < im_num_row
-                im_num_col = im_num_col + 1;
-            else
-                im_num_row = im_num_row + 1;
-            end
-            NOI = im_num_col*im_num_row;
-        end
+        [NOI,im_num_row,im_num_col] = findGridSize(p.Results.number_of_images,pixWidth,pixHeight,overlap_x,overlap_y);
         LRC = lower_right_corner;
         ULC = ...
             [LRC(1) - (im_num_col-1)*(pixWidth-overlap_x)*mmhandle.core.getPixelSizeUm,...
@@ -494,4 +468,40 @@ grid.im_num_col = im_num_col;
 grid.im_num_row = im_num_row;
 grid.overlap_x = overlap_x;
 grid.overlap_y = overlap_y;
+end
+
+%% findGridSize
+% It is suprising to find that the dimensions of a grid are not trivially
+% determined when only given the number of images to collect, because an
+% arbitrary constraint is required. An algorithm was created to find the
+% _most square_ grid that comes closest to the desired number of images
+% without going beyond a height to width ratio with a lower bound of
+% _minRatio_ and an upper bound of _maxRatio_. If the suggested dimensions
+% contain fewer images then the desired amount, an extra row or column is
+% added until the grid contains more images than the desired amount. Later
+% on, the final row is trimmed to match the exact of images requested.
+%
+% The size of the image must also be taken into consideration. For example,
+% if the height to with ratio is 2/3 a square imaging space would consist
+% of subgrids with 3 rows and 2 columns. Also, for this reason, the overlap
+% is taken into consideration.
+%
+% Finally, the minimum aspect ratio is set to be 9:16 and the width of the
+% grid will always have more length than the height.
+function [NOI,im_num_row,im_num_col] = findGridSize(numberOfImages,pixWidth,pixHeight,overlap_x,overlap_y)
+imageRatio = (pixHeight - overlap_y)/(pixWidth-overlap_x); %height/width
+widthCandidates = floor((sqrt(numberOfImages*imageRatio./(linspace(0.5625/imageRatio,1,10))))/imageRatio);
+objectiveArray = mod(numberOfImages,widthCandidates);
+[~,ind] = min(objectiveArray);
+im_num_col = floor(widthCandidates(ind)/imageRatio); %un-adjust for the size of a non-square image
+im_num_row = floor(numberOfImages/im_num_col);
+NOI = im_num_col*im_num_row;
+while NOI < numberOfImages
+    if im_num_col < im_num_row
+        im_num_col = im_num_col + 1;
+    else
+        im_num_row = im_num_row + 1;
+    end
+    NOI = im_num_col*im_num_row;
+end
 end
--- SuperMDA/SuperMDA_readme_publish.m
+++ SuperMDA/SuperMDA_readme_publish.m
@@ -6,6 +6,6 @@ clear('options_doc');
 options_doc.codeToEvaluate = 'SuperMDA_database2CellProfilerCSV(fullfile(mfilepath,''testfiles'',''example_database.txt''),fullfile(''some'',''path'',''some'',''where''),fullfile(mfilepath,''testfiles''))';
 options_doc.maxOutputLines = 0;
 publish(fullfile(mfilepath,'SuperMDA_database2CellProfilerCSV.m'),options_doc);
-options_doc.codeToEvaluate = 'SuperMDA_database2CellProfilerCSV(fullfile(mfilepath,''testfiles'',''example_database.txt''),fullfile(''some'',''path'',''some'',''where''),fullfile(mfilepath,''testfiles''))';
+options_doc.codeToEvaluate = 'SuperMDA_grid_maker(mmhandle,''centroid'',[0,0,0],''number_of_images'',11)';
 options_doc.maxOutputLines = 0;
-publish(fullfile(mfilepath,'SuperMDA_database2CellProfilerCSV.m'),options_doc);
+publish(fullfile(mfilepath,'SuperMDA_grid_maker.m'),options_doc);
