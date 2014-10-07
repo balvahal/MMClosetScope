49a9c7e8df60ba5edb7076e1ff418513566b076b
karhohs-kishonyscope <gandalfisarockstar@gmail.com>
2014-10-07 01:21:03 -0400

    cleaning up the metadata creation
    a memory leak was discovered and traced back, but without 100 percent
    certainty, to the database of metadata. This has been fixed by
    generating a text file for each image.

--- SuperMDA/SuperMDAPilot_method_update_database.m
+++ SuperMDA/SuperMDAPilot_method_update_database.m
@@ -13,6 +13,28 @@ smdaP.mm.getXYZ;
 myGroupOrder = smdaP.itinerary.orderOfGroup;
 myPositionOrder = smdaP.itinerary.orderOfPosition(g);
 mySettingsOrder = smdaP.itinerary.orderOfSettings(g,p);
+varNames = {'channel_name',...
+    'filename',...
+    'group_label',...
+    'position_label',...
+    'binning',...
+    'channel_number',...
+    'continuous_focus_offset',...
+    'continuous_focus_bool',...
+    'exposure',...
+    'group_number',...
+    'group_order',...
+    'matlab_serial_date_number',...
+    'position_number',...
+    'position_order',...
+    'settings_number',...
+    'settings_order',...
+    'timepoint',...
+    'x',...
+    'y',...
+    'z',...
+    'z_order',...
+    'image_description'};
 myNewDatabaseRow = {...
     smdaP.itinerary.channel_names{smdaP.itinerary.settings_channel(s)},... %channel_name
     smdaP.itinerary.database_filenamePNG,... %filename
@@ -36,4 +58,56 @@ myNewDatabaseRow = {...
     smdaP.mm.pos(3),...%     smdaP.itinerary.group(g).position(p).settings(s).z_origin_offset + ...%     smdaP.itinerary.group(g).position(p).settings(s).z_stack(z) + ...%     smdaP.itinerary.group(g).position(p).xyz(t,3),... %z
     z,... %the order of zstack from bottom to top
     smdaP.database_imagedescription}; %image_description
-smdaP.database(smdaP.runtime_imagecounter,:) = myNewDatabaseRow;
+%%
+%
+metadataFilename = sprintf('g%d_s%d_w%d_t%d_z%d.txt',g,p,s,t,z);
+fileID = fopen(fullfile(smdaP.itinerary.png_path,metadataFilename),'w');
+formatSpecHeader = strcat(...
+    '%s\t',... %channel_name
+    '%s\t',... %filename
+    '%s\t',... %group_label
+    '%s\t',... %position_label
+    '%s\t',... %binning
+    '%s\t',... %channel_number
+    '%s\t',... %continuous_focus_offset
+    '%s\t',... %continuous_focus_bool
+    '%s\t',... %exposure
+    '%s\t',... %group_number
+    '%s\t',... %group_order
+    '%s\t',... %matlab_serial_date_number
+    '%s\t',... %position_number
+    '%s\t',... %position_order,
+    '%s\t',... %settings_number
+    '%s\t',... %settings_order
+    '%s\t',... %timepoint
+    '%s\t',...%smdaP.itinerary.group(g).position(p).xyz(t,1),... %x
+    '%s\t',...%smdaP.itinerary.group(g).position(p).xyz(t,2),... %y
+    '%s\t',...%     smdaP.itinerary.group(g).position(p).settings(s).z_origin_offset + ...%     smdaP.itinerary.group(g).position(p).settings(s).z_stack(z) + ...%     smdaP.itinerary.group(g).position(p).xyz(t,3),... %z
+    '%s\t',... %the order of zstack from bottom to top
+    '%s\n'); %image_description
+formatSpecDatabaseRow = strcat(...
+    '%s\t',... %channel_name
+    '%s\t',... %filename
+    '%s\t',... %group_label
+    '%s\t',... %position_label
+    '%d\t',... %binning
+    '%d\t',... %channel_number
+    '%f\t',... %continuous_focus_offset
+    '%d\t',... %continuous_focus_bool
+    '%f\t',... %exposure
+    '%d\t',... %group_number
+    '%d\t',... %group_order
+    '%g\t',... %matlab_serial_date_number
+    '%d\t',... %position_number
+    '%d\t',... %position_order,
+    '$d\t',... %settings_number
+    '%d\t',... %settings_order
+    '%d\t',... %timepoint
+    '%f\t',...%smdaP.itinerary.group(g).position(p).xyz(t,1),... %x
+    '%f\t',...%smdaP.itinerary.group(g).position(p).xyz(t,2),... %y
+    '%f\t',...%     smdaP.itinerary.group(g).position(p).settings(s).z_origin_offset + ...%     smdaP.itinerary.group(g).position(p).settings(s).z_stack(z) + ...%     smdaP.itinerary.group(g).position(p).xyz(t,3),... %z
+    '%d\t',... %the order of zstack from bottom to top
+    '%s\n'); %image_description
+fprintf(fileID,formatSpecHeader,varNames{:});
+fprintf(fileID,formatSpecDatabaseRow,myNewDatabaseRow{:});
+fclose(fileID);
