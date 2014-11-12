b95e848359c3f5f7446e23ea4294e12a5da4e96a
Kyle Wayne Karhohs <gandalfisarockstar@gmail.com>
2014-11-12 15:53:05 -0500

    the master database will no longer grow in a loop

--- SuperMDA/SuperMDA_makeMasterDatabase.m
+++ SuperMDA/SuperMDA_makeMasterDatabase.m
@@ -12,13 +12,12 @@ mydir = dir(fullfile(path,'RAW_DATA'));
 % find all of the text files
 mydir = mydir(cellfun(@(x) ~isempty(regexp(x,'.txt$','start')),{mydir(:).name}));
 mylength = length(mydir);
-mytable = readtable(fullfile(path,'RAW_DATA',mydir(1).name),'Delimiter','\t');
-if length(mydir) > 1
-    for i = 2:length(mydir)
-        mytable = vertcat(mytable,readtable(fullfile(path,'RAW_DATA',mydir(i).name),'Delimiter','\t')); %#ok<AGROW>
-        fprintf('%2.2f: %s\r\n',i/mylength*100,mydir(i).name);
-    end
+mytable = cell(length(mydir),1);
+for i = 1:length(mydir)
+    mytable{i} = readtable(fullfile(path,'RAW_DATA',mydir(i).name),'Delimiter','\t');
+    fprintf('%2.2f: %s\r\n',i/mylength*100,mydir(i).name);
 end
+mytable = vertcat(mytable{:});
 databasefilename = fullfile(path,'smda_database.txt');
 writetable(mytable,databasefilename,'Delimiter','tab');
 end
