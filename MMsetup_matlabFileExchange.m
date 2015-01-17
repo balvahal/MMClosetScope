4987a4951866e0a011318191c5bac6423a9a1c4b
Kyle Wayne Karhohs <gandalfisarockstar@gmail.com>
2015-01-17 11:53:23 -0500

    matlab file exchange

--- MMsetup_matlabFileExchange.m
+++ MMsetup_matlabFileExchange.m
@@ -5,12 +5,22 @@ end
 
 %% Download files from the MATLAB FILE EXCHANGE
 % # JSONlab - a toolbox to encode/decode JSON files in MATLAB/Octave
+% # twitty - Interface-class to access the Twitter REST API v1.1.
 %%% JSONlab
 %
 filename = fullfile(mfilepath,'matlab_file_exchange','JSONlab.zip');
-url = 'http://www.mathworks.com/matlabcentral/fileexchange/downloads/58936';
+url = 'http://www.mathworks.com/matlabcentral/fileexchange/submissions/33381/v/16/download/zip';
 websave(filename,url);
 unzip(fullfile(mfilepath,'matlab_file_exchange','JSONlab.zip'),...
     fullfile(mfilepath,'matlab_file_exchange','JSONlab')...
+    );
+delete(filename);
+%%% twitty
+%
+filename = fullfile(mfilepath,'matlab_file_exchange','twitty.zip');
+url = 'http://www.mathworks.com/matlabcentral/fileexchange/submissions/34837/v/6/download/zip';
+websave(filename,url);
+unzip(fullfile(mfilepath,'matlab_file_exchange','twitty.zip'),...
+    fullfile(mfilepath,'matlab_file_exchange','twitty')...
     );
 delete(filename);
