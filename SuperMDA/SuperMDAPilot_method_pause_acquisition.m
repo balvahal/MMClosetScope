48147e7f88269bf24062027f885aefcf5d249e7a
karhohs <gandalfisarockstar@gmail.com>
2015-03-31 14:41:52 -0400

    remove redundant files

--- SuperMDA/SuperMDAPilot_method_pause_acquisition.m
+++
@@ -1,6 +0,0 @@
-%%
-%
-function [smdaPilot] = SuperMDAPilot_method_pause_acquisition(smdaPilot)
-if smdaPilot.running_bool
-    smdaPilot.pause_bool = true;
-end
--- SuperMDA/SuperMDAPilot_method_resume_acquisition.m
+++
@@ -1,6 +0,0 @@
-%%
-%
-function [smdaPilot] = SuperMDAPilot_method_resume_acquisition(smdaPilot)
-if smdaPilot.running_bool
-    smdaPilot.pause_bool = false;
-end
