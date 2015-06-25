f02864d68afa6a9f2070860f0c0a35123b62f0fc
Kyle Wayne Karhohs <gandalfisarockstar@gmail.com>
2015-06-25 13:12:25 -0400

    new 3x3 grid with z-stack

--- SuperMDA/SuperMDAItineraryTimeFixed_settings_function.m
+++ SuperMDA/SuperMDAItineraryTimeFixed_settings_function.m
@@ -36,26 +36,26 @@ if smdaPilot.itinerary.settings_z_stack_upper_offset(k) - smdaPilot.itinerary.se
         smdaPilot.database_z_number = a;
         move_z(z_stack(a));
         smdaPilot.snap;
-    smdaPilot.itinerary.database_filenamePNG = sprintf('g%d_%s_s%d_%s_w%d_%s_t%d_z%d.tiff',i,smdaPilot.itinerary.group_label{i},j,strcat(smdaPilot.itinerary.position_label{j},''),k,smdaPilot.itinerary.channel_names{smdaPilot.itinerary.settings_channel(k)},smdaPilot.t,smdaPilot.database_z_number);
-    smdaPilot.mm.core.waitForSystem();
-    try
-        imwrite(smdaPilot.mm.I,fullfile(smdaPilot.itinerary.png_path,smdaPilot.itinerary.database_filenamePNG),'tiff','Compression','none','WriteMode','overwrite');
-    catch
-        if obj.twitterBool
-            try
-                obj.twitter.updateStatus(sprintf('Error in writing image to disk from the %s microscope.',obj.computerName));
-            catch
-                disp('Twitter Error!');
-            end
-        end
-        smdaPilot.snap;
+        smdaPilot.itinerary.database_filenamePNG = sprintf('g%d_%s_s%d_%s_w%d_%s_t%d_z%d.tiff',i,smdaPilot.itinerary.group_label{i},j,strcat(smdaPilot.itinerary.position_label{j},''),k,smdaPilot.itinerary.channel_names{smdaPilot.itinerary.settings_channel(k)},smdaPilot.t,smdaPilot.database_z_number);
         smdaPilot.mm.core.waitForSystem();
-        imwrite(smdaPilot.mm.I,fullfile(smdaPilot.itinerary.png_path,smdaPilot.itinerary.database_filenamePNG),'tiff','Compression','none','WriteMode','overwrite');
-    end
-    %% Update the database
-    %
-    smdaPilot.update_database;
-    smdaPilot.mm.core.waitForSystem();
+        try
+            imwrite(smdaPilot.mm.I,fullfile(smdaPilot.itinerary.png_path,smdaPilot.itinerary.database_filenamePNG),'tiff','Compression','none','WriteMode','overwrite');
+        catch
+            if obj.twitterBool
+                try
+                    obj.twitter.updateStatus(sprintf('Error in writing image to disk from the %s microscope.',obj.computerName));
+                catch
+                    disp('Twitter Error!');
+                end
+            end
+            smdaPilot.snap;
+            smdaPilot.mm.core.waitForSystem();
+            imwrite(smdaPilot.mm.I,fullfile(smdaPilot.itinerary.png_path,smdaPilot.itinerary.database_filenamePNG),'tiff','Compression','none','WriteMode','overwrite');
+        end
+        %% Update the database
+        %
+        smdaPilot.update_database;
+        smdaPilot.mm.core.waitForSystem();
     end
 else
     %% Snap and Image if there is no z-stack
